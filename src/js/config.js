f2f312c63cf79df23a5b015e26ab13d190378030
Mykhailo-Zhuk <mishazhuk1@gmail.com>
2022-11-06 17:46:06 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SECONDS = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'adfa6743-2c1d-4179-a827-d5d01fbf3bd0';
+export const MODAL_CLOSE_SEC = 2.5;
